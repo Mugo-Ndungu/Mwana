c5803ffca81b521f8647726601be797cc10200c4
Wanjiku <Wanjiku>
2019-06-18 18:59:07 +0300

    Migrate commit

--- mzazi/admin.py
+++ mzazi/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile,Posts,tag
 
-# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    filter_horizontal =('tags',)
+    
+admin.site.register(Profile)
+admin.site.register(Posts)
+admin.site.register(tag)
+
